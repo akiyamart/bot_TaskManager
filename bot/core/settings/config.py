85ae0a73ce4386d954a5acebe306301e354bd403
akiyamart <akiyamah@yandex.ru>
2024-09-24 11:25:52 +0300

    fix

--- bot/core/settings/config.py
+++ bot/core/settings/config.py
@@ -3,9 +3,9 @@ import os
 from aiogram import Bot, Dispatcher
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '7670322043:AAHw-6_DIFO2v-R47oBUcn3MC81jOO-xZHU')
-    DATABASE = os.getenv("DATABASE_DSN", "postgresql+asyncpg://postgres:holdmn@localhost:5436/task_manager_postgres")
-    # BOT_TOKEN = os.getenv("BOT_TOKEN", "6676724790:AAEvd6ofLoMiAJ8ks1YvR1I2jhmSg9n1vMw")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    DATABASE = os.getenv("DATABASE_DSN", "")
+    # BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 bot = Bot(token=Config.BOT_TOKEN)
 dp = Dispatcher()
